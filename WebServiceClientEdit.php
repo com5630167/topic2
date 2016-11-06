79dca2228615879969e41cf143c9d16e8ed14953
com5630167 <com5630167@gmail.com>
2016-11-07 01:59:00 +0700

    topic2

--- WebServiceClientEdit.php
+++ WebServiceClientEdit.php
@@ -16,14 +16,14 @@
 	// FOR CALL NUSOAP
 	require("lib/nusoap.php");
 ?>
-<input type="button" id="BACK" value="BACK" onclick="location.href = 'http://ec2-35-162-119-116.us-west-2.compute.amazonaws.com/topic2/index.html';" class="btn btn-default"/>
+<input type="button" id="BACK" value="BACK" onclick="location.href = 'http://ec2-35-161-234-98.us-west-2.compute.amazonaws.com/topic2/index.html';" class="btn btn-default"/>
 <!-- EDIT SERVICE -->
 <h1> Edit Book By Name Service </h1>
 <?php
   	if($_POST['submit_edit'] == "Submit") {
 		$from_name=$_POST['from_name'];
 		$to_name=$_POST['to_name'];
-        $client = new nusoap_client("http://ec2-35-162-119-116.us-west-2.compute.amazonaws.com/topic2/WebServiceServer.php?wsdl",true); 
+        $client = new nusoap_client("http://ec2-35-161-234-98.us-west-2.compute.amazonaws.com/topic2/WebServiceServer.php?wsdl",true); 
         $params = array(
 			'from_name'=>$from_name,
 			'to_name'=>$to_name
